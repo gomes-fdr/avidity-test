1434e1b7fb32c9ae67ae96041e3ed3a313cb1e7b
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-05-31 22:16:50 -0300

    FIX comment

--- app/user.py
+++ app/user.py
@@ -11,7 +11,7 @@ def get_param_from_url(url, param_name):
 @bp_user.route('/')
 @bp_user.route('/<page>')
 def index(page=1):
-    api_token = 'd8a4b668b66a5c34e25f7ecadf46063eb7721dff' # This token is automaticly revoke when I commit it.
+    api_token = 'd8a4b668b66a5c34e25f7ecadf46063eb7721dff'
     api_uri = 'https://api.github.com'
     repo_url = api_uri + '/repos/turicas/brasil.io'
     commits_url = api_uri + '/repos/turicas/brasil.io/commits' + '?page={}'.format(page)
