6002a1927f9a868ba86eb469fadf8d9476b03d5b
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-06-05 14:52:43 -0300

    CHANGED: Remove unnecessary function to catch number of pages

--- app/user.py
+++ app/user.py
@@ -1,4 +1,5 @@
 import os
+import re
 import requests
 import json
 from flask import Blueprint
@@ -6,33 +7,31 @@ from flask import render_template
 
 bp_user = Blueprint('user', __name__)
 
-def get_param_from_url(url, param_name):
-    return [i.split("=")[-1] for i in url.split("?", 1)[-1].split("&") if i.startswith(param_name + "=")][0]
-
 @bp_user.route('/')
 @bp_user.route('/<page>')
 def index(page=1):
     api_token = os.environ['TOKEN_API']
     api_uri = 'https://api.github.com'
     repo_url = api_uri + '/repos/turicas/brasil.io'
-    commits_url = api_uri + '/repos/turicas/brasil.io/commits' + '?page={}'.format(page)
+    commits_url = repo_url + '/commits' + '?page={}'.format(page)
 
     try:
         response = requests.get(repo_url, headers={'Authorization': 'token ' + api_token})
     except requests.ConnectionError:
-        return "Connection Error"
+        return 'Connection Error', 500
 
     repo = json.loads(response.text)
 
     try:
         response = requests.get(commits_url, headers={'Authorization': 'token ' + api_token})
     except requests.ConnectionError:
-        return "Connection Error"
+        return 'Connection Error', 500
 
     link = response.headers.get('link')
     if link is not None:
-        total = link.split(',')
-        pages = int(get_param_from_url(total[1].split(';')[0].replace('>', ''), 'page'))
+        expr = 'page=([^&>]+)'
+        list_links = link.split(',')
+        pages = int(re.findall(expr, list_links[1])[0])
     else:
         pages=1
 
